6659edf6d96773a1959b294889a4757935d656f0
Teal <git@teal.is>
2024-12-26 15:49:58 +0100

    Change interactive.sh according to build.sh - remove sudo, more precise lookup

--- interactive.sh
+++ interactive.sh
@@ -4,18 +4,20 @@ COMMIT_ID=$(git rev-parse --short HEAD)
 IMAGE_NAME="yocto-librescoot:${COMMIT_ID}"
 
 mkdir -p yocto
-sudo chown 999:999 yocto
 
-if ! sudo docker images | grep -q "${COMMIT_ID}"; then
-    echo "Building Docker image ${IMAGE_NAME}..."
-    sudo docker build -t "${IMAGE_NAME}" ./docker
+if ! docker images "${IMAGE_NAME}" | grep -q "${COMMIT_ID}"; then
+    echo "Docker image ${IMAGE_NAME} not found, building..."
+    docker build \
+        --build-arg UID=$(id -u) \
+        --build-arg GID=$(id -g) \
+        -t "${IMAGE_NAME}" \
+        ./docker
 else
     echo "Using existing Docker image ${IMAGE_NAME}."
 fi
 
-sudo docker run -it --rm \
+docker run -it --rm \
     -v "$(pwd)/yocto:/yocto" \
     --name yocto-build \
     --entrypoint /bin/bash \
     "${IMAGE_NAME}"
-
